3579cef74cc46a4cde4e2a52da2fa61442ae566d
soumava <soumavadas06@gmail.com>
2024-10-19 20:20:25 +0530

    Dynamic variable assign

--- Dynamic Web Application/Page Links/index.php
+++ Dynamic Web Application/Page Links/index.php
@@ -1,3 +1,24 @@
 <?php
 
-require "index.view.php";
+$header = "Home";
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+// echo $_SERVER['REQUEST_URI'];
+
+// dd($_SERVER);
+
+if ($_SERVER['REQUEST_URI'] === '/') {
+    echo 'bg-gray-900 text-white';
+} else {
+    echo 'text-gray-300';
+}
+
+require "views/index.view.php";
