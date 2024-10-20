38c983a54475e37155409bd639f46e667235dcbe
soumava <soumavadas06@gmail.com>
2024-10-21 02:19:17 +0530

    PHP routes completed

--- Dynamic Web Application/index.php
+++ Dynamic Web Application/index.php
@@ -1,13 +1,4 @@
 <?php
 
 require 'functions.php';
-
-$uri = $_SERVER['REQUEST_URI'];
-
-if($uri === '/') {
-    require 'controlers/index.php';
-} else if($uri === '/about') {
-    require 'controlers/about.php';
-} else if ($uri === '/contact') {
-    require 'controlers/contact.php';
-}
+require 'router.php';
