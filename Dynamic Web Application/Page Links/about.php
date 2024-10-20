28858aa5ea4564c9aec5f15734d9c1ba267c90a9
soumava <soumavadas06@gmail.com>
2024-10-20 12:25:19 +0530

    Superglobals

--- Dynamic Web Application/Page Links/about.php
+++ Dynamic Web Application/Page Links/about.php
@@ -1,3 +1,7 @@
-<?php 
+<?php
+
+require "functions.php";
+
+$heading = 'About Us';
 
 require "views/about.view.php";
--- Dynamic Web Application/Page Links/index.php
+++ Dynamic Web Application/Page Links/index.php
@@ -1,24 +1,20 @@
 <?php
 
-$header = "Home";
+require "functions.php";
 
-function dd($value)
-{
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-
-    die();
-}
+$heading = "Home";
 
 // echo $_SERVER['REQUEST_URI'];
 
 // dd($_SERVER);
 
-if ($_SERVER['REQUEST_URI'] === '/') {
-    echo 'bg-gray-900 text-white';
-} else {
-    echo 'text-gray-300';
-}
+// if ($_SERVER['REQUEST_URI'] === '/') {
+//     echo 'bg-gray-900 text-white';
+// } else {
+//     echo 'text-gray-300';
+// }
+
+// echo $_SERVER['REQUEST_URI'] === '/' ? 'bg-gray-900 text-white' : 'text-gray-300';
+
 
 require "views/index.view.php";
