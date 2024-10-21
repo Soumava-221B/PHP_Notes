d4b5e3c2f85ac9dcfbd68c2c8563ee8927808260
soumava <soumavadas06@gmail.com>
2024-10-21 16:59:08 +0530

    connecting to MySQL database and fetching data

--- Dynamic Web Application/index.php
+++ Dynamic Web Application/index.php
@@ -1,4 +1,50 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+
+// class Person {
+//     public $name;
+//     public $age;
+    
+//     public function breath() {
+//         echo $this -> name . ' is breathing!';
+//     }
+// }
+
+// $person = new Person();
+
+// $person -> name = 'John Doe';
+// $person -> age = 25;
+
+// dd($person -> name);
+// $person -> breath();
+
+
+// connect to our MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+
+$username = 'root';
+$password = 'test123';
+
+// pdo instant
+$pdo = new PDO($dsn, $username, $password);
+
+// try {
+//     $pdo = new PDO($dsn, $username, $password);
+//     // Set the PDO error mode to exception
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connected successfully";
+// } catch (PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+// }
+
+$statement = $pdo -> prepare("SELECT * FROM posts");
+$statement -> execute();
+
+$posts = $statement -> fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
