50b1f07f344b51fbc2ac63a4b0b19c6c90d6c73b
soumava <soumavadas06@gmail.com>
2024-11-08 21:43:18 +0530

    added delete POST request

--- Dynamic Web Application/controllers/notes/show.php
+++ Dynamic Web Application/controllers/notes/show.php
@@ -7,16 +7,31 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-authorize($note['user_id'] === $currentUserId);
+    $currentUserId = 1;
 
-// if($note['user_id'] !== $currentUserId) {
-//     abort(Response::FORBIDDEN);
-// }
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    // form was submitted. delete the current note.
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
 
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    header('location: /notes');
+    exit();
+} else {
+
+    $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+
+}
