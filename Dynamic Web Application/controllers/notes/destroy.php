46ec13939cd3bcdb22d3ba30df31383e1b95562e
soumava <soumavadas06@gmail.com>
2024-11-11 20:14:10 +0530

    refactored the create and delete notes routes

---
+++ Dynamic Web Application/controllers/notes/destroy.php
@@ -0,0 +1,21 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_POST['id']])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+// form was submitted. delete the current note.
+$db->query('delete from notes where id = :id', [
+    'id' => $_POST['id']
+]);
+
+
+header('location: /notes');
+exit();
--- Dynamic Web Application/controllers/notes/show.php
+++ Dynamic Web Application/controllers/notes/show.php
@@ -7,31 +7,13 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $currentUserId = 1;
+$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
 
-    $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
+authorize($note['user_id'] === $currentUserId);
 
-    authorize($note['user_id'] === $currentUserId);
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
 
-    // form was submitted. delete the current note.
-    $db->query('delete from notes where id = :id', [
-        'id' => $_GET['id']
-    ]);
-
-
-    header('location: /notes');
-    exit();
-} else {
-
-    $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
-
-    authorize($note['user_id'] === $currentUserId);
-
-    view("notes/show.view.php", [
-        'heading' => 'Note',
-        'note' => $note
-    ]);
-
-}
