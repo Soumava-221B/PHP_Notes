81a52888a086b5225cd9bf70ea707849f76758dc
soumava <soumavadas06@gmail.com>
2024-11-12 22:15:49 +0530

    building a basic container

--- Dynamic Web Application/public/index.php
+++ Dynamic Web Application/public/index.php
@@ -11,6 +11,8 @@ spl_autoload_register(function ($class) {
     require (base_path("{$class}.php"));
 }); 
 
+require base_path('bootstrap.php');
+
 $router = new \Core\Router();
 
 $routes = require base_path('routes.php');
@@ -19,17 +21,3 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
-
-
-// $id = $_GET['id'];
-// when accepting user input through query string or form never inline it as a part of the sql query because if you do that it will create an SQL injection Vulnerabilities 
-
-// $query = "SELECT * FROM posts WHERE id = {$id}";  
-// insted you can replace it with an ?
-// $query = "SELECT * FROM posts WHERE id = ?";
-
-// var_dump($query);
-
-// $posts = $db->query($query, [$id])->fetch();
-
-// var_dump($posts);
