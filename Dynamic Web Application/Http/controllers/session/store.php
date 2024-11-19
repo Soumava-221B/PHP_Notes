5ab29a32293126ae1b43b70ed0591893ec6a8b03
soumava <soumavadas06@gmail.com>
2024-11-19 21:52:47 +0530

    Refactored and extracted the form validation

--- Dynamic Web Application/Http/controllers/session/store.php
+++ Dynamic Web Application/Http/controllers/session/store.php
@@ -4,27 +4,21 @@
 
 use Core\App;
 use Core\Database;
-use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email';
-}
+$form = new LoginForm();
 
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password';
-}
-
-if (! empty($errors)) {
+if (! $form->validate($email, $password)) {
     return view('session/create.view.php', [
-        'errors' => $errors
+        'errors' => $forms->errors()
     ]);
 }
+
 
 // match the credentials
 $user = $db->query('SELECT * FROM users WHERE email = :email', [
